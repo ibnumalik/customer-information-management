8eb2aba5259daf5adc792782bb215f410e3dac3b
Shaiful <shaiful.malik@myeg.com.my>
2020-05-23 06:59:47 +0800

    Add routes for customer crud

--- api/src/customer/customer.controller.ts
+++ api/src/customer/customer.controller.ts
@@ -1,9 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { CustomerService } from './customer.service';
+import { Customer } from './customer.entity';
 
 @Controller('customer')
 export class CustomerController {
+  constructor(private customerService: CustomerService) {}
+
   @Get()
-  index() {
-    return 'This action will return customer';
+  index(): Promise<Customer[]> {
+    return this.customerService.findAll();
+  }
+
+  @Post('create')
+  async create(@Body() customerData: Customer): Promise<any> {
+    return this.customerService.create(customerData);
+  }
+
+  @Put(':id/update')
+  async update(@Param('id') id, @Body() customerData: Customer): Promise<any> {
+    customerData.id = Number(id);
+    console.log(`Update #${customerData.id}`);
+    return this.customerService.update(customerData);
+  }
+
+  @Delete(':id/delete')
+  async delete(@Param('id') id): Promise<any> {
+    return this.customerService.delete(id);
   }
 }
