46ed8af8c04bc26eb654df803cf0a7a99e96b8b5
Shaiful <shaiful.malik@myeg.com.my>
2020-05-23 01:00:32 +0800

    Add crud operation in service

--- api/src/customer/customer.service.ts
+++ api/src/customer/customer.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Customer } from './customer.entity';
+import { Repository, UpdateResult, DeleteResult } from 'typeorm';
 
 @Injectable()
-export class CustomerService {}
+export class CustomerService {
+  constructor(
+    @InjectRepository(Customer)
+    private customerRepository: Repository<Customer>,
+  ) {}
+
+  async findAll(): Promise<Customer[]> {
+    return await this.customerRepository.find();
+  }
+
+  async create(customer: Customer): Promise<Customer> {
+    return await this.customerRepository.save(customer);
+  }
+
+  async update(customer: Customer): Promise<UpdateResult> {
+    return await this.customerRepository.update(customer.id, customer);
+  }
+
+  async delete(id: number): Promise<DeleteResult> {
+    return await this.customerRepository.delete(id);
+  }
+}
