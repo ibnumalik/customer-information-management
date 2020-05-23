63277671a7ee088fe6294354592bae4bd24c114d
Shaiful <shaiful.malik@myeg.com.my>
2020-05-23 14:23:52 +0800

    Add customer component and route

---
+++ ui/src/views/Customer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container mx-auto mt-16">
+    <h1 class="mb-6 text-4xl">Customers</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import axios from 'axios';
+
+export default Vue.extend({
+  data() {
+    return {
+      customers: []
+    };
+  },
+  mounted() {
+    axios.get('http://localhost:3003/customer').then(res => {
+      this.customers = res.data;
+    });
+  }
+});
+</script>
