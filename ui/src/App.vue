f3a4f53925f11f8a544a194eb2a68c5fec624112
Shaiful <shaiful.malik@myeg.com.my>
2020-05-23 23:29:06 +0800

    Refresh ui components

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,17 +1,32 @@
 <template>
   <div id="app">
-    <Navbar />
-
-    <router-view />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import Navbar from './components/Navbar';
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-export default {
-  components: {
-    Navbar
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
   }
-};
-</script>
+}
+</style>
