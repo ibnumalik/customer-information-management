cf03b200e55e606031f1573e4514660072b8f0e8
Shaiful <shaiful.malik@myeg.com.my>
2020-05-26 01:16:59 +0800

    Add validation pipe to customer entity

--- api/src/customer/customer.entity.ts
+++ api/src/customer/customer.entity.ts
@@ -1,19 +1,25 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { IsNotEmpty, IsEmail } from 'class-validator';
 
 @Entity()
 export class Customer {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @IsNotEmpty()
   @Column()
   name: string;
 
+  @IsNotEmpty()
+  @IsEmail()
   @Column()
   email: string;
 
+  @IsNotEmpty()
   @Column()
   phone: string;
 
+  @IsNotEmpty()
   @Column()
   address: string;
 }
