3b471c6ddf2a95ce86d25a1a6ced822f04af2f7c
Shaiful <shaiful.malik@myeg.com.my>
2020-05-26 01:40:02 +0800

    Use mdi js for smaller icon bundle

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,11 +1,7 @@
 <template>
   <v-app>
     <v-app-bar app color="blue-grey darken-1" dark flat>
-      <v-app-bar-nav-icon
-        class="tw-hidden"
-        :hidden="$vuetify.breakpoint.lgAndUp"
-        @click.stop="drawer = !drawer"
-      ></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon :hidden="$vuetify.breakpoint.lgAndUp" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
 
       <div class="tw-flex tw-items-center">
         <router-link to="/" class="brand-link">
@@ -25,34 +21,38 @@
       </div>
     </v-app-bar>
 
-    <div class="lg:tw-hidden">
-      <v-navigation-drawer v-model="drawer" app color="blue-grey" dark>
-        <v-list nav>
-          <v-list-item two-line>
-            <v-list-item-content>
-              <v-list-item-title>Customer Management</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      color="blue-grey"
+      dark
+      :hidden="$vuetify.breakpoint.lgAndUp"
+    >
+      <v-list nav>
+        <v-list-item two-line>
+          <v-list-item-content>
+            <v-list-item-title>Customer Management</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
 
-          <v-divider></v-divider>
+        <v-divider></v-divider>
 
-          <v-list-item to="/">
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item>
+        <v-list-item to="/">
+          <v-list-item-icon>
+            <v-icon>{{mdiHome}}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Home</v-list-item-title>
+        </v-list-item>
 
-          <v-list-item to="/customer">
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
+        <v-list-item to="/customer">
+          <v-list-item-icon>
+            <v-icon>{{mdiAccount}}</v-icon>
+          </v-list-item-icon>
 
-            <v-list-item-title>Customer</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-    </div>
+          <v-list-item-title>Customer</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-content class="bg-light-grey">
       <router-view></router-view>
@@ -61,11 +61,15 @@
 </template>
 
 <script>
+import { mdiHome, mdiAccount } from "@mdi/js";
+
 export default {
   name: "App",
 
   data: () => ({
-    drawer: false
+    drawer: false,
+    mdiHome,
+    mdiAccount
   })
 };
 </script>
