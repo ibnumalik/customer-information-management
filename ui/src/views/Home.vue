006f5386cf73fe1e31bd8b4e8a4131635bc6b26a
Shaiful <shaiful.malik@myeg.com.my>
2020-05-25 23:18:04 +0800

    Update home page responsive design

--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -1,23 +1,25 @@
 <template>
-  <div class="home d-flex">
-    <div class="hero section d-flex">
+  <div class="home lg:tw-flex">
+    <div
+      class="hero tw-py-8 tw-px-4 tw-mb-8 lg:tw-mb-0 lg:tw-w-1/2 tw-flex tw-items-center lg:tw-px-12"
+    >
       <h1
-        class="display-4 blue-grey--text text--darken-3 align-self-center text-right"
+        class="blue-grey--text text--darken-3 tw-text-3xl lg:tw-text-7xl tw-font-light tw-leading-none lg:tw-text-right tw-tracking-tight"
       >
         Manage your customer.
       </h1>
     </div>
-    <div class="content d-flex flex-column justify-center section">
-      <p class="display-1">Built API using nestjs</p>
-      <ul class="mb-4">
+    <div class="tw-px-4 lg:tw-px-12 tw-flex tw-justify-center tw-flex-col">
+      <p class="tw-text-2xl lg:tw-text-4xl">Built API using nestjs</p>
+      <ul class="mb-4 tw-list-disc">
         <li>Register new customer</li>
         <li>View customer detail</li>
         <li>Update customer detail</li>
         <li>Delete customer detail</li>
       </ul>
 
-      <p class="display-1">Build frontend SPA using vuejs</p>
-      <ul>
+      <p class="tw-text-2xl lg:tw-text-4xl">Build frontend SPA using vuejs</p>
+      <ul class="tw-list-disc">
         <li>User should be able to register new customer</li>
         <li>User should be able to see listing of registered customers</li>
         <li>
@@ -43,13 +45,5 @@ export default {
 
 .hero {
   background: #78909c;
-}
-
-.content {
-}
-
-.section {
-  width: 50%;
-  padding: 32px;
 }
 </style>
