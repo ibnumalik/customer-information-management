3f0dc8800f478904ed82eb182fff5b8222406f00
Shaiful <shaiful.malik@myeg.com.my>
2020-05-26 00:04:51 +0800

    Add drawer nav for mobile

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <v-app>
     <v-app-bar app color="blue-grey darken-1" dark flat>
-      <div class="d-flex align-center">
+      <div class="tw-flex tw-items-center">
+        <div class="lg:tw-hidden">
+          <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        </div>
         <router-link to="/" class="brand-link">
           <h1 class="brand">Customer Information Management</h1>
         </router-link>
@@ -10,10 +13,43 @@
       <v-spacer></v-spacer>
 
       <div class="tw-hidden lg:tw-block">
-        <v-btn text to="/"> <span class="mr-2">Home</span> </v-btn>
-        <v-btn text to="customer"> <span class="mr-2">Customer</span> </v-btn>
+        <v-btn text to="/">
+          <span class="mr-2">Home</span>
+        </v-btn>
+        <v-btn text to="customer">
+          <span class="mr-2">Customer</span>
+        </v-btn>
       </div>
     </v-app-bar>
+
+    <div class="lg:tw-hidden">
+      <v-navigation-drawer v-model="drawer" app color="blue-grey" dark>
+        <v-list nav>
+          <v-list-item two-line>
+            <v-list-item-content>
+              <v-list-item-title>Customer Management</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-divider></v-divider>
+
+          <v-list-item to="/">
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item to="/customer">
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-title>Customer</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+    </div>
 
     <v-content class="bg-light-grey">
       <router-view></router-view>
@@ -23,11 +59,11 @@
 
 <script>
 export default {
-  name: 'App',
+  name: "App",
 
   data: () => ({
-    //
-  }),
+    drawer: false
+  })
 };
 </script>
 
